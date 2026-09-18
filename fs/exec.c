a6b5b2f52ec873f26b26f2b68e1081469ca01c98
eepy-loftenth <loftenth@proton.me>
2026-09-18 19:32:30 +0530

    KernelSU-Next: add legacy root support

--- fs/exec.c
+++ fs/exec.c
@@ -1895,12 +1895,18 @@ out_ret:
 }
 
 static int do_execveat_common(int fd, struct filename *filename,
-			      struct user_arg_ptr argv,
-			      struct user_arg_ptr envp,
-			      int flags)
+							  struct user_arg_ptr argv,
+							  struct user_arg_ptr envp,
+							  int flags)
 {
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
+
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+							   void *argv, void *envp, int *flags);
+#endif
 
 int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
@@ -1911,11 +1917,14 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 }
 
 int do_execve(struct filename *filename,
-	const char __user *const __user *__argv,
-	const char __user *const __user *__envp)
+			  const char __user *const __user *__argv,
+			  const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1943,6 +1952,9 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
--- fs/read_write.c
+++ fs/read_write.c
@@ -305,6 +305,12 @@ loff_t vfs_llseek(struct file *file, loff_t offset, int whence)
 }
 EXPORT_SYMBOL(vfs_llseek);
 
+#ifdef CONFIG_KSU
+extern bool ksu_vfs_read_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
+													 char __user **buf_ptr, size_t *count_ptr);
+#endif
+
 SYSCALL_DEFINE3(lseek, unsigned int, fd, off_t, offset, unsigned int, whence)
 {
 	off_t retval;
@@ -580,6 +586,11 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
+
+	#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_handle_sys_read(fd, &buf, &count);
+	#endif
 
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
